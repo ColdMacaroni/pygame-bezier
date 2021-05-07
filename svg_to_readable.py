f848a75559f4519665809d72348bce315c618c1d
muffin <squishysparrow7.7@gmail.com>
2021-05-08 01:08:41 +1200

    Start on open function

---
+++ svg_to_readable.py
@@ -0,0 +1,18 @@
+#!/bin/env python3
+# This script will take an svg file and output its path in a format
+# that draw_bezier can read.
+from sys import argv
+from Bea
+def get_text():
+    file = argv[1]
+    with open(file, 'r') as svg:
+        text = svg.readlines()
+
+
+def main():
+    file = argv[1]
+    with open()
+
+
+if __name__ == "__main__":
+    main()

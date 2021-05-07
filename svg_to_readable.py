d567df9d045aac0c22e9259332db997ab5138a26
muffin <squishysparrow7.7@gmail.com>
2021-05-08 01:33:24 +1200

    plan

--- svg_to_readable.py
+++ svg_to_readable.py
@@ -1,17 +1,62 @@
 #!/bin/env python3
 # This script will take an svg file and output its path in a format
 # that draw_bezier can read.
-from sys import argv
-from Bea
-def get_text():
-    file = argv[1]
-    with open(file, 'r') as svg:
-        text = svg.readlines()
+
+def get_nums():
+    text = input("Pasted the contents inside of the d attribute of path: ")
+
+    values = text.split()
+
+    check = 0
+
+    if values[0] == 'm':
+        del values[0]
+        check += 1
+
+    if values[-1] == 'z':
+        del values[-1]
+        check += 1
+
+    if values[1] == 'c':
+        del values[1]
+        check += 1
+
+    print(values)
+
+    letters = 0
+    for item in values:
+        if item.isalpha():
+            letters += 1
+
+    if letters or check != 3:
+        raise Exception('Format must be composed of only relative cubic bezier curves')
+
+    return values
+
+
+def svg_str_to_tuple(str_list):
+    """
+    Turns the list of svg coords into tuples instead of strings
+    :param str_list:
+    :return:
+    """
+    new_list = []
+    for item in str_list:
+        new_list.append(eval("({})".format(item)))
+
+    return new_list
 
 
 def main():
-    file = argv[1]
-    with open()
+    print(1)
+    txt_list = get_nums()
+    coord_list = svg_str_to_tuple(txt_list)
+
+    # Split the list into sections of 3, expect for the first which will be of 4.
+    # then on each section of 3 insert at the start the last item of the last section
+    # Add the coordinates of the last item to the other 3 in the section
+    # Rinse and repeat
+    # Should be enough for the other .py to work
 
 
 if __name__ == "__main__":
